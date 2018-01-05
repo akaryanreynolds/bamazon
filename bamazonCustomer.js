f341a51139cfdcb87a721057892cbf13731a0fd5
akaryanreynolds <ryanreynolds.ca@gmail.com>
2018-01-04 20:26:46 -0800

    Displaying items
    Items now show when the js is started

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,8 +11,18 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){
     console.log("Connectes as id: "+connection.threadId);
+    displayItems();
 })
 
 function displayItems (){
-    
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            console.log(" - - - - - - - - - - - - - - - ");
+            console.log("item #: " + res[i].item_id);
+            console.log("item for sale: " + res[i].product_name);
+            console.log("price of item: $" + res[i].price);
+        }
+        connection.end();
+    });
 }
