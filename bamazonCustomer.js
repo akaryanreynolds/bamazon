61e6499cc4d21704b124e8c46a62061ad2e18562
akaryanreynolds <ryanreynolds.ca@gmail.com>
2018-01-05 21:07:41 -0800

    Working bamazon
    The app is working but is only updating the stock quantity with the amount that the user wants to purchage not subtracting the amount.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,6 +10,7 @@ var connection = mysql.createConnection({
 })
 
 connection.connect(function (err) {
+    if (err) throw err;
     console.log("Connectes as id: " + connection.threadId);
     displayItems();
 })
@@ -23,6 +24,69 @@ function displayItems() {
             console.log("item for sale: " + res[i].product_name);
             console.log("price of item: $" + res[i].price);
         }
-        connection.end();
+        // connection.end();
+        buyItem();
     });
+}
+
+function buyItem() {
+    inquirer.prompt([{
+        type: "list",
+        name: "buy_item",
+        message: "Would you like to buy and item?",
+        choices: ["YES", "NO"]
+    }
+    ]).then(function (n) {
+        if (n.buy_item === "YES") {
+            chooseItem()
+        }
+        else if (n.buy_item === "NO") {
+            connection.end();
+        }
+    })
+}
+
+function chooseItem() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "item_num",
+            message: "What item # would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        }
+    ]).then(function (n) {
+        console.log("Buying items...\n");
+        connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: n.quantity
+                },
+                {
+                    item_id: n.item_num
+                }
+            ],
+            function(error) {
+                if (error) throw err;
+                console.log("Item purchased and quantity updated successfully!");
+                buyItem();
+            }
+        )
+    })
 }
