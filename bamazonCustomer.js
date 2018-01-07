e89f2717c5a4bde861e4c4c2cd90e04c6af468c5
akaryanreynolds <ryanreynolds.ca@gmail.com>
2018-01-07 14:20:24 -0800

    Homework complete
    Done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,10 +19,11 @@ function displayItems() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log(" - - - - - - - - - - - - - - - ");
+            console.log("\n");
             console.log("item #: " + res[i].item_id);
             console.log("item for sale: " + res[i].product_name);
             console.log("price of item: $" + res[i].price);
+            console.log("-----------------------\n");
         }
         // connection.end();
         buyItem();
@@ -41,6 +42,7 @@ function buyItem() {
             chooseItem()
         }
         else if (n.buy_item === "NO") {
+            console.log("\nCome back when you're ready to buy.\nGood Bye!\n")
             connection.end();
         }
     })
@@ -52,41 +54,45 @@ function chooseItem() {
             type: "input",
             name: "item_num",
             message: "What item # would you like to buy?",
-            validate: function(value) {
+            validate: function (value) {
                 if (isNaN(value) === false) {
-                  return true;
+                    return true;
                 }
                 return false;
-              }
+            }
         },
         {
             type: "input",
             name: "quantity",
             message: "How many would you like to buy?",
-            validate: function(value) {
+            validate: function (value) {
                 if (isNaN(value) === false) {
-                  return true;
+                    return true;
                 }
                 return false;
-              }
+            }
         }
     ]).then(function (n) {
-        console.log("Buying items...\n");
-        connection.query(
-            "UPDATE products SET ? WHERE ?",
-            [
-                {
-                    stock_quantity: n.quantity
-                },
-                {
-                    item_id: n.item_num
-                }
-            ],
-            function(error) {
-                if (error) throw err;
-                console.log("Item purchased and quantity updated successfully!");
+
+        connection.query("SELECT * FROM products WHERE ?", { item_id: n.item_num }, function (err, data) {
+            if (err) throw err;
+            var productData = data[0];
+
+            console.log("Buying items...\n");
+            if (n.quantity <= productData.stock_quantity) {
+                connection.query("UPDATE products SET stock_quantity = " + (productData.stock_quantity - n.quantity) + " WHERE item_id = " + n.item_num, function (err, data) {
+                    if (err) throw err;
+                    console.log("Your order has been placed! Your total charged to your account is $" + productData.price * n.quantity + "\nGoodbye");
+                    console.log("-----------------------\n");
+                    // End the database connection
+                    connection.end();
+                })
+            } else {
+                console.log("Not enough product in stock. Returning to start.");
+                console.log("-----------------------\n");
+                // displayItems();
                 buyItem();
             }
-        )
+        })
     })
 }
